a8749c015595c5913182f722d33c148a6e9bed54
Rong Ying <kohrongying@gmail.com>
2019-10-22 14:34:54 +0800

    Refactor code

---
+++ src/About.svelte
@@ -0,0 +1,44 @@
+<script>
+	const src = "pic.png";
+</script>
+
+<style>
+	#left-fixed {
+		background-color: salmon;
+		height: 100vh;
+		width: 30vw;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		position: fixed;
+	}
+	#contactme {
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+	}
+	.contact-link {
+		width: 50px;
+		height: 50px;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+</style>
+
+<section id="left-fixed">
+		<img {src} alt="pic" width=100 />
+		<div id="contactme">
+		<a class="contact-link" href="https://github.com/kohrongying">
+			<img src="github-icon.png" alt='github' width=40 />
+		</a>
+		<a class="contact-link" href="mailto:kohrongying@gmail.com?Subject=Hello">
+			<img src="mail-icon.png" alt='mail' width=25 />
+		</a>
+		<a class="contact-link" href="https://sg.linkedin.com/in/rongyingkoh">
+			<img src="lnkedin-icon.png" alt='linkedin' width=30 />
+		</a>
+		</div>
+	</section>
---
+++ src/Works.svelte
@@ -0,0 +1,48 @@
+<script>
+	import { repos } from './repos.js'
+</script>
+
+<style>
+	#right-fixed {
+		width: 70vw;
+		float: right;
+	}
+	#card-container {
+		margin: 20px;
+		display: flex;
+		flex-wrap: wrap;
+		padding: 25px;
+		justify-content: space-between;
+	}
+	.card {
+		width: 40%;
+		margin: 15px;
+		padding: 15px;
+		box-shadow: #cccccc63 1px 1px 6px 2px;
+	}
+	.title-container {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+</style>
+
+<section id="right-fixed">
+  <div id="card-container">
+    {#each repos as repo}
+      <div class="card">
+
+        <div class="title-container">
+
+          <a href={repo.url}>
+            {repo.name}
+          </a>
+            <a href={repo.github_url}>
+              <img src="github-icon.png" alt='github' width=40 />
+            </a>
+        </div>
+        <span>{repo.desc}</span>
+      </div>
+    {/each}
+  </div>
+</section>
